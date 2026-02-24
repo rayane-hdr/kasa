e3dee8e4d8de78fedd0f5739b0622f42ec4bc945
rayane <rayaneelhadri1@gmail.com>
2026-02-24 20:58:55 +0100

    Fix ScrollTop problem

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import ScrollToTop from "../ScrollTop";
 
 export default function Layout() {
   return (
     <div className="page">
       <Header />
       <main className="page__content">
+        <ScrollToTop />
         <Outlet />
       </main>
       <Footer />
--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -5,8 +5,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
-    
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
   }, [pathname]);
 
   return null;
