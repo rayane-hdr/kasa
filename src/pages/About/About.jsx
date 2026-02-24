bae2da41fdfcdb18a358e43bfec287e7d2220a52
rayane <rayaneelhadri1@gmail.com>
2026-02-24 20:25:02 +0100

    Fix collapse about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,35 +1,15 @@
-import Collapse from "../../components/Collapse/Collapse";
-import "./About.scss";
 import Banner from "../../components/Banner/Banner";
+import Collapse from "../../components/Collapse/Collapse";
 import aboutImage from "../../assets/about-banner.jpg";
+import items from "../../data/about.json";
+
+import "./About.scss";
 
 export default function About() {
-  const items = [
-    {
-      title: "Fiabilité",
-      content:
-        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
-    },
-    {
-      title: "Respect",
-      content:
-        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou perturbateur entraînera une exclusion de notre plateforme.",
-    },
-    {
-      title: "Service",
-      content:
-        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
-    },
-    {
-      title: "Sécurité",
-      content:
-        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services.",
-    },
-  ];
-
   return (
-    <main className="about">
+    <div className="about">
       <Banner image={aboutImage} dark />
+      
       <section className="about__content">
         {items.map((item) => (
           <Collapse key={item.title} title={item.title}>
@@ -37,6 +17,6 @@ export default function About() {
           </Collapse>
         ))}
       </section>
-    </main>
+    </div>
   );
 }
